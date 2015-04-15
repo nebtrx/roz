3fcde8668f26d596470ca9ab1c1e6ba112ed3b8f
nostromo\Neb <neb.trx@gmail.com>
2015-04-15 15:43:20 -0500

    Adding T4 for Enum - Lookup tables mapping

--- Roz.Data.Model/Entities/Enums.cs
+++ Roz.Data.Model/Entities/Enums.cs
@@ -1,55 +1,197 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿                            
 namespace Roz.Data.Model.Entities
 {
-    public enum DiscountType
-    {
-        Progressive,
-        ItemsPrice
-    }
+	        
+	#region DiscountType  
 
-    public enum EventStatus
-    {
-        Inactive,
-        Active,
-        Archived
-    }
+	/// <summary>
+	/// DiscountType auto generated enumeration
+	/// </summary>
+	public enum DiscountType
+	{
+	  
+        /// <summary>
+		/// Progressive configuration setting.
+		/// </summary>
+		Progressive = 1,
 
-    public enum AllocationType
-    {
-        GeneralAllocation,
-        SeatingPlan,
-    }
+	  
+        /// <summary>
+		/// ItemsPrice configuration setting.
+		/// </summary>
+		Itemsprice = 2
+		
+	}
 
-    public enum FeeType
-    {
-        Flat
-    }
+	#endregion
+        
+	#region EventStatus  
 
-    public enum EventCategory
-    {
-        Bussiness = 1,
-        Cinema = 2,
-        FoodEvent = 3,
-        Sports = 4,
-        Social = 5,  // Pub, club, bars, 
-        Music = 6,
-        Theather = 7,
-        Tour = 8,
-        Conference = 9,
-        Weeding = 10,
-        FundRaising = 11,
-        Other = 12,
-    }
+	/// <summary>
+	/// EventStatus auto generated enumeration
+	/// </summary>
+	public enum EventStatus
+	{
+	  
+        /// <summary>
+		/// Inactive configuration setting.
+		/// </summary>
+		Inactive = 1,
 
-    public enum SeatStatus
-    {
-        Reserved,
-        Confirmed,
-        Empty
-    }
+	  
+        /// <summary>
+		/// Active configuration setting.
+		/// </summary>
+		Active = 2,
+
+	  
+        /// <summary>
+		/// Archived configuration setting.
+		/// </summary>
+		Archived = 3
+		
+	}
+
+	#endregion
+        
+	#region AllocationType  
+
+	/// <summary>
+	/// AllocationType auto generated enumeration
+	/// </summary>
+	public enum AllocationType
+	{
+	  
+        /// <summary>
+		/// GeneralAllocation configuration setting.
+		/// </summary>
+		Generalallocation = 1
+		
+	}
+
+	#endregion
+        
+	#region FeeType  
+
+	/// <summary>
+	/// FeeType auto generated enumeration
+	/// </summary>
+	public enum FeeType
+	{
+	  
+        /// <summary>
+		/// Flat configuration setting.
+		/// </summary>
+		Flat = 1
+		
+	}
+
+	#endregion
+        
+	#region EventCategory  
+
+	/// <summary>
+	/// EventCategory auto generated enumeration
+	/// </summary>
+	public enum EventCategory
+	{
+	  
+        /// <summary>
+		/// Bussiness configuration setting.
+		/// </summary>
+		Bussiness = 1,
+
+	  
+        /// <summary>
+		/// Cinema configuration setting.
+		/// </summary>
+		Cinema = 2,
+
+	  
+        /// <summary>
+		/// FoodEvent configuration setting.
+		/// </summary>
+		Foodevent = 3,
+
+	  
+        /// <summary>
+		/// Sports configuration setting.
+		/// </summary>
+		Sports = 4,
+
+	  
+        /// <summary>
+		/// Social configuration setting.
+		/// </summary>
+		Social = 5,
+
+	  
+        /// <summary>
+		/// Music configuration setting.
+		/// </summary>
+		Music = 6,
+
+	  
+        /// <summary>
+		/// Theather configuration setting.
+		/// </summary>
+		Theather = 7,
+
+	  
+        /// <summary>
+		/// Tour configuration setting.
+		/// </summary>
+		Tour = 8,
+
+	  
+        /// <summary>
+		/// Conference configuration setting.
+		/// </summary>
+		Conference = 9,
+
+	  
+        /// <summary>
+		/// Weeding configuration setting.
+		/// </summary>
+		Weeding = 10,
+
+	  
+        /// <summary>
+		/// Other configuration setting.
+		/// </summary>
+		Other = 11
+		
+	}
+
+	#endregion
+        
+	#region SeatStatus  
+
+	/// <summary>
+	/// SeatStatus auto generated enumeration
+	/// </summary>
+	public enum SeatStatus
+	{
+	  
+        /// <summary>
+		/// Reserved configuration setting.
+		/// </summary>
+		Reserved = 1,
+
+	  
+        /// <summary>
+		/// Confirmed configuration setting.
+		/// </summary>
+		Confirmed = 2,
+
+	  
+        /// <summary>
+		/// Empty configuration setting.
+		/// </summary>
+		Empty = 3
+		
+	}
+
+	#endregion
 }
+

5481f85b368181d890386700becbab74b7654976
nostromo\Neb <neb.trx@gmail.com>
2015-04-17 12:30:58 -0500

    Adding Async Exceptions Logging

--- Roz.WebApp/Global.asax.cs
+++ Roz.WebApp/Global.asax.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Elmah;
 using Roz.Data.EntityFramework;
 
 namespace Roz.WebApp
